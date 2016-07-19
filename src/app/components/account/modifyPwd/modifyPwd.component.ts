1329101ab782bdd2f892ca2e1efb5e70340ac13d
ToBePlus <float1101@gmail.com>
2016-07-19 10:40:02 +0800

    修改密码
    修改门店

---
+++ src/app/components/account/modifyPwd/modifyPwd.component.ts
@@ -0,0 +1,83 @@
+import { Component, Input, Output } from '@angular/core';
+import { ROUTER_DIRECTIVES, Router, RouteSegment } from '@angular/router';
+import { Http, Response, HTTP_PROVIDERS } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+import { FORM_DIRECTIVES, ControlGroup, FormBuilder, Control } from '@angular/common';
+import * as moment from 'moment';
+import * as _ from 'lodash';
+import { Md5 } from 'ts-md5/dist/md5';
+import { UserApi, CommonApi } from 'client';
+import { MainLogoComponent, PageFooterComponent, NavbarComponent, MenusComponent } from 'common';
+
+@Component({
+	moduleId: module.id,
+	selector: 'modify-pwd',
+	template: require('./modifyPwd.html'),
+	styles: [require('./modifyPwd.scss')],
+	directives: [ROUTER_DIRECTIVES, FORM_DIRECTIVES, NavbarComponent, MenusComponent],
+	providers: [HTTP_PROVIDERS, UserApi, CommonApi, Md5 ]
+})
+
+export class ModifyPwdComponent {
+	pwd: any;
+
+    mpwdForm: ControlGroup;
+    active: Boolean;
+
+	constructor(private router: Router, private fb: FormBuilder, private params: RouteSegment, private uApi: UserApi, private cApi: CommonApi) {
+		this.mpwdForm = fb.group({
+            'oldPassword': [''],
+            'password': [''],
+            'rePassword': ['']
+        });
+        this.active = true;
+        
+	}
+
+	ngOnInit() {
+		// this.mpwdForm.controls.rePassword.isEq = 1;
+	}
+	
+    onSave() {
+        // console.log(this.mpwdForm);
+        // if(this.mpwdForm.controls.rePassword.isEq && this.mpwdForm.valid ) {
+        //     let params = this.mpwdForm.value;
+        //     this.uApi.userChangePwdPost(params.oldPassword,params.password,params.rePassword).subscribe(data => {
+        //         console.log('this.cApi.userChangePwdPost()');
+        //         console.dir(data);
+        //     })
+        // } else {
+        //     alert('你输入的信息有误');
+        //     console.log(this.mpwdForm);
+        //     return false;
+        // }
+        
+    }
+    onReset() {
+        // this.mpwdForm = this.fb.group({
+        //     'oldPassword': [''],
+        //     'password': [''],
+        //     'rePassword': ['']
+        // });
+        // this.mpwdForm.controls.rePassword.isEq = 1;
+        // console.log(this.mpwdForm);
+        // this.active = false;
+        // setTimeout(() => this.active = true, 0);
+    }
+    onCancel() {
+        this.router.navigate(['/my-account']);
+    }
+    onRepasswordBlur () {
+        // if(!this.mpwdForm.controls.rePassword.valid) {
+        //     this.mpwdForm.controls.rePassword.isEq = 1;
+        //     return;
+        // }
+        // this.mpwdForm.controls.rePassword.isEq = this.mpwdForm.controls.rePassword.value == this.mpwdForm.controls.password.value ? 1 : 0;
+    }
+    onRepasswordFocus () {
+        // this.mpwdForm.controls.rePassword.isEq = 1;
+    }
+    
+	
+}
