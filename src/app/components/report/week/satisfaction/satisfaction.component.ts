3e46f7e6a359a4777b97fb944bde605cabc6ac3c
Rain.xia <rain.xia@mytianhui.com>
2016-07-22 18:25:32 +0800

    report

--- src/app/components/report/week/satisfaction/satisfaction.component.ts
+++ src/app/components/report/week/satisfaction/satisfaction.component.ts
@@ -7,7 +7,7 @@ import { FORM_DIRECTIVES, ControlGroup, FormBuilder, Control } from '@angular/co
 import * as moment from 'moment';
 import * as _ from 'lodash';
 import { Md5 } from 'ts-md5/dist/md5';
-import { UserApi, CommonApi } from 'client';
+import { UserApi, CommonApi, ReportApi } from 'client';
 import { MainLogoComponent, PageFooterComponent, NavbarComponent, MenusComponent } from 'common';
 
 @Component({
@@ -16,20 +16,41 @@ import { MainLogoComponent, PageFooterComponent, NavbarComponent, MenusComponent
 	template: require('./satisfaction.html'),
 	styles: [require('./satisfaction.scss')],
 	directives: [ROUTER_DIRECTIVES, FORM_DIRECTIVES, NavbarComponent, MenusComponent],
-	providers: [HTTP_PROVIDERS, UserApi, CommonApi, Md5 ]
+	providers: [HTTP_PROVIDERS, UserApi, CommonApi, Md5, ReportApi ]
 })
 
 export class ReportWeekSatisfactionComponent {
-	
-	constructor(private router: Router, fb: FormBuilder, params: RouteSegment, private uApi: UserApi, private cApi: CommonApi) {
+	start: string;
+	end: string;
+	percent: number = 0;
+	shopGoods: string[] = [];
+	shopBads: string[] = [];
+	employeeGoods: any = [];
+	employeeBads: any = [];
+	improvements: any = [];
+	constructor(private router: Router, fb: FormBuilder, params: RouteSegment, private rApi: ReportApi) {
 		console.log(params);
-		
+		this.end = moment().format('YYYY-MM-DD');
+		this.start = (moment().subtract(7, 'days')).format('YYYY-MM-DD');
 	}
 
 	ngOnInit() {
-		
+		this.getWeekReport();
 	}
 	
+	getWeekReport() {
+		this.rApi.reportAttitudeGet(this.start, this.end).subscribe(data => {
+			if(data.data && data.data.length) {
+				const dd = data.data[0];
+				this.percent = dd.percent;
+				this.shopGoods = dd.shopGoods;
+				this.shopBads = dd.shopBads;
+				this.employeeGoods = dd.employeeGoods;
+				this.employeeBads = dd.employeeBads;
+				this.improvements = dd.improvements;
+			}
+		}, err => console.error(err) );
+	}
 
 	
 }
