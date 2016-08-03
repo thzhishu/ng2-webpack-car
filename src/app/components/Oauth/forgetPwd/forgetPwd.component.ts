1b0dbf49dd1827b14eeef156a1177ab558e0b03c
ToBePlus <float1101@gmail.com>
2016-08-03 18:55:40 +0800

    测试

--- src/app/components/Oauth/forgetPwd/forgetPwd.component.ts
+++ src/app/components/Oauth/forgetPwd/forgetPwd.component.ts
@@ -20,35 +20,27 @@ import { MainLogoComponent, PageFooterComponent } from 'common';
 })
 
 export class ForgetPwdComponent {
-  fpForm: ControlGroup;
-  newPwdForm: ControlGroup;
   zone: any;
-  user: any = {};
   seekDisabeld: number = 0;
-  seekBtnTitle: number = 0;
+  seekTime: number = 0;
+  seekBtnTitle: any = '发送验证码';
   next: number = 1;
   loading: number = 0;
   sign: string;
   img: any;
   diff: number = 0;
+  timeout: any;
+  errorPhoneCode: string;
+  openProtocol: boolean = false;
+  fp:any = {};
 
-  constructor(private router: Router, fb: FormBuilder, private route: ActivatedRoute, private uApi: UserApi, private cApi: CommonApi) {
-    this.zone = new NgZone({ enableLongStackTrace: false }); //事务控制器
-
-    //表单验证
-    this.fpForm = fb.group({
-      'phone': [''],
-      'rnd': [''],
-      'code': [''],
-    });
-    this.newPwdForm = fb.group({
-      'pwd': [''],
-      'checkPwd': [''],
-    });
+  constructor(private router: Router, private fb: FormBuilder, private route: ActivatedRoute, private uApi: UserApi, private cApi: CommonApi) {
+    this.zone = new NgZone({ enableLongStackTrace: true }); // 事务控制器
   }
-  //初始化
+  // 初始化
   ngOnInit() {
     this.getCodeImg();
+    // this.initForm();
   }
   /**
    * 获取图片验证码
@@ -81,20 +73,29 @@ export class ForgetPwdComponent {
       return;
     }
     this.seekDisabeld = 1;
-    this.seekBtnTitle = 60;
-    this.getPhoneCode(phone, rnd);
-
-    //倒计时
-    let timeout = window.setInterval(() => {
-      this.zone.run(() => {
-        if (this.seekBtnTitle > 0) {
-          this.seekBtnTitle--;
-        } else {
-          this.seekBtnTitle = null;
-          this.seekDisabeld = 0;
-        }
-      });
-    }, 1000);
+    this.seekTime = 60;
+    this.getPhoneCode(phone, rnd).subscribe(data => {
+      if (data.meta.code !== 200) {
+        this.getCodeImg();
+        this.errorWin(data.error.message);
+        this.seekBtnTitle = '重新发送';
+        this.seekDisabeld = 0;
+      } else {
+        this.seekBtnTitle = '发送验证码';
+        //倒计时
+        this.timeout = window.setInterval(() => {
+          this.zone.run(() => {
+            if (this.seekTime > 0) {
+              this.seekTime--;
+              this.seekBtnTitle = this.seekTime + 's';
+            } else {
+              this.seekBtnTitle = '重新发送';
+              this.seekDisabeld = 0;
+            }
+          });
+        }, 1000);
+      }
+    });
   }
   /**
    * 请求手机验证码
@@ -105,37 +106,35 @@ export class ForgetPwdComponent {
   getPhoneCode(phone: string = '', rnd: string = '') {
     let salt = 'thzs0708';
     this.sign = Md5.hashStr(phone + rnd + salt, false).toString();
-    this.uApi.userPasswordSmsPost(phone, rnd, this.sign).subscribe(data => {
-
-    })
+    return this.uApi.userPasswordSmsPost(phone, rnd, this.sign);
   }
   //验证手机号
   onCheckPhone() {
-    if (!this.fpForm.valid) {
-      alert('你输入的信息有误.不能完成找回密码');
-      return false;
-    }
-    let params = this.fpForm.value;
+    this.loading = 1;
+    let params = this.fp;
     params.uuid = this.uApi.defaultHeaders.get('uuid');
-    //code: string, phone: string, uuid: string,
+    // code: string, phone: string, uuid: string,
     this.cApi.commonCodeVerifyGet(params.code, params.phone, params.uuid).subscribe(data => {
       if (data.meta.code === 200) {
         this.next = 2;
         this.sign = data.data.sign;
       } else {
-        alert(data.error.message);
+        this.errorWin(data.error.message);
+        this.getCodeImg();
       }
-    })
+      this.loading = 0;
+    });
   }
 
-  //重置密码
+  errorWin(message) {
+    this.openProtocol = true;
+    this.errorPhoneCode = message === '短信验证码超时，导致userId不存在' ? '你离开的时间过长,请重新操作' : message;
+  }
+
+  // 重置密码
   onEditPwd() {
     this.loading = 1;
-    if (!this.newPwdForm.valid) {
-      alert('你输入的信息有误.不能完成重置密码');
-      return false;
-    }
-    let params = this.newPwdForm.value;
+    let params = this.fp;
     if (params.pwd !== params.checkPwd) {
       alert('两次密码不一致');
       this.diff = 1;
@@ -144,17 +143,27 @@ export class ForgetPwdComponent {
     } else {
       this.diff = 0;
     }
-    //password: string, rePassword: string, sign: string
+    // password: string, rePassword: string, sign: string
     this.uApi.userUpdatePwdPost(Md5.hashStr(params.pwd, false).toString(), Md5.hashStr(params.checkPwd, false).toString(), this.sign)
       .subscribe(data => {
         this.loading = 0;
         if (data.meta.code === 200) {
-          this.next = 1;
           alert('密码修改成功');
+          this.router.navigate(['/login-min']);
+          // this.next = 1;
         } else {
-          alert(data.error.message);
+          this.next = 1;
+          this.errorWin(data.error.message);
+          this.getCodeImg();
         }
-      })
+      });
+  }
+
+  onClose() {
+    this.openProtocol = false;
+    if(this.errorPhoneCode === '短信验证码超时，导致userId不存在'){
+      this.router.navigate(['/login-min']);
+    }
   }
 
   toHome() {
